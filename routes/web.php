f846d32bbfe1f488c250a107918fc7225ac44827
Riccardo <riky.volto@live.com>
2023-10-25 19:20:54 +0200

    fine prova

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'title' => 'ciao',
+        'message' => 'lorem ipsum'
+    ];
+    return view('ciao', $data);
 });
